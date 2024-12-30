c826300385dbe04efdc74a987258f5fb018a26af
Davis Giang <davis.giang@powin.com>
2024-12-29 23:35:07 -0800

    Revert "added chakra ui"
    This reverts commit 376922c5d918f8a543e007a6fdc25700133d2717.

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { ChakraProvider } from "@chakra-ui/provider"
-import './index.css';
-import App from './App.jsx';
-
-console.log(ChakraProvider);
-
-// 2. build your theme and config
-const theme = {
-  // ... your system-ui theme
-  config: {
-    useSystemColorMode: false, // or true
-    initialColorMode: "light", // or "dark"
-    cssVarPrefix: "chakra", // any string
-  }
-}
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <ChakraProvider theme = {theme}>
-      <App />
-    </ChakraProvider>
-);
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
