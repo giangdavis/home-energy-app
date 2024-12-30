376922c5d918f8a543e007a6fdc25700133d2717
Davis Giang <davis.giang@powin.com>
2024-12-29 23:19:21 -0800

    added chakra ui

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from "@chakra-ui/provider"
+import './index.css';
+import App from './App.jsx';
+
+console.log(ChakraProvider);
+
+// 2. build your theme and config
+const theme = {
+  // ... your system-ui theme
+  config: {
+    useSystemColorMode: false, // or true
+    initialColorMode: "light", // or "dark"
+    cssVarPrefix: "chakra", // any string
+  }
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ChakraProvider theme = {theme}>
+      <App />
+    </ChakraProvider>
+);
